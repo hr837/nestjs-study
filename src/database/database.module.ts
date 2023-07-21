12a81ef0fd52213eee8eae3fb1c5eb39bc21ea37
huangrui <hr837@163.com>
2023-07-21 18:52:25 +0800

    fix: 修改数据库配置

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseService } from './database.service';
-import { AppconfigModule } from 'src/appconfig/appconfig.module';
 
 @Module({
   imports: [
-    AppconfigModule,
     TypeOrmModule.forRootAsync({
       useClass: DatabaseService,
     }),
